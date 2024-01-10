531aa14f889eaf35fb6cf07d211c4abe33637bf8
AlbedoLinguine <yogevcr@gmail.com>
2024-01-09 19:32:07 -0600

    yay intake

---
+++ src/main/java/frc/robot/commands/RunIntakeCommand.java
@@ -0,0 +1,32 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Intake;
+
+public class RunIntakeCommand extends Command {
+
+    final boolean forward;
+    final Intake intake;
+
+    @Override
+    public void initialize() {
+        
+    }
+
+    public RunIntakeCommand(boolean forward, Intake intake){
+        this.forward = forward;
+        this.intake = intake;
+
+        if(forward){
+            intake.inTake();
+        }
+        else {
+            intake.outTake();
+        }
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        intake.run(0);
+    }
+}
---
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -0,0 +1,23 @@
+package frc.robot.subsystems;
+
+import java.util.List;
+
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class Intake {
+    private Spark intakeSparky;
+
+    public void run(double power){
+        intakeSparky.set(power);
+    }
+
+    public void inTake(){
+        run(1);
+    }
+
+    public void outTake(){
+        run(-1);
+    }
+
+}
