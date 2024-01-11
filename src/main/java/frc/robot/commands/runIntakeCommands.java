74a55cc301fc3f5bb0ebbb535a1d0485d583805b
micw1417 <80129528+micw1417@users.noreply.github.com>
2024-01-11 17:33:08 -0600

    h

--- src/main/java/frc/robot/commands/runIntakeCommands.java
+++ src/main/java/frc/robot/commands/runIntakeCommands.java
@@ -2,7 +2,6 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Intake;
-import frc.robot.subsystems.Intake.Modes;
 
 public class runIntakeCommands extends Command {
     final boolean in;
