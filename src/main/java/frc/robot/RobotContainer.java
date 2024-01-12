f7894ec8e72ec1f7aa29d8842cfe45265aeeff6c
AlbedoLinguine <yogevcr@gmail.com>
2024-01-11 19:28:18 -0600

    operator xobx button a b

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,16 +5,26 @@
 package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
+import frc.robot.commands.RunIntakeCommand;
+import frc.robot.subsystems.Intake;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.team5431.titan.core.joysticks.CommandXboxController;
-// import frc.team5431.titan.core.joysticks.CommandXboxController;
 
 public class RobotContainer {
 
+  private final Systems systems = new Systems();
   private final CommandXboxController m_driverController =
       new CommandXboxController(OperatorConstants.kDriverControllerPort);
+  private final CommandXboxController driver = new CommandXboxController(0);
+  private final CommandXboxController operator = new CommandXboxController(1);
+
+  private void configureBindings() {
+    operator.a().whileTrue(new RunIntakeCommand(true, systems.getIntake())); //remember to replace null
+    operator.b().whileTrue(new RunIntakeCommand(false, systems.getIntake())); //remember to replace null
+  }
 
   public RobotContainer() {
+
   }
 
   public Command getAutonomousCommand() {
--- src/main/java/frc/robot/Systems.java
+++ src/main/java/frc/robot/Systems.java
@@ -1,19 +1,25 @@
 package frc.robot;
 
+import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.Vision; 
 
 public class Systems{
 
     private Vision vision;
+    private Intake intake;
 
     public Systems() {
         vision = new Vision();
-
+        intake = new Intake();
         
     }
 
     public Vision getVision(){
         return vision;
+    }
+
+    public Intake getIntake(){
+        return intake;
     }
 
 
