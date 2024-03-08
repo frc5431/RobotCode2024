e99aeddfa803a88bf8680f9bf570b0c11cda2452
Rubyboat <52091447+Rubyboat1207@users.noreply.github.com>
2024-03-07 21:43:16 -0600

    speakerscore done until testing

--- src/main/java/frc/robot/commands/auton/SpeakerScore.java
+++ src/main/java/frc/robot/commands/auton/SpeakerScore.java
@@ -4,7 +4,10 @@ import java.util.Optional;
 
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.Commands;
@@ -13,16 +16,17 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants;
 import frc.robot.commands.DriveLockedRotCommand;
 import frc.robot.commands.RunAnglerCommand;
+import frc.robot.commands.RunManipulatorCommand;
 import frc.robot.commands.RunAnglerCommand.TerminationCondition;
 import frc.robot.subsystems.Angler;
 import frc.robot.subsystems.Manipulator;
 import frc.robot.subsystems.PheonixDrivebase;
+import frc.robot.subsystems.Manipulator.Modes;
 
 public class SpeakerScore extends SequentialCommandGroup {
     public SpeakerScore(Manipulator intake, Manipulator shooter, Angler angler, Angler pivot,
             PheonixDrivebase drivebase) {
 
-        
         Alliance alliance = DriverStation.getAlliance().get();
         try {
             var tags = AprilTagFieldLayout.loadFromResource(AprilTagFields.k2024Crescendo.m_resourceFile);
@@ -34,12 +38,26 @@ public class SpeakerScore extends SequentialCommandGroup {
                 return;
             }
 
+            Pose3d targetPosition = tags.getTagPose(alliance == Alliance.Blue ? 7 : 4).get();
 
+            targetPosition = new Pose3d(targetPosition.getX(), targetPosition.getY(),
+                    targetPosition.getZ() + Units.feetToMeters(1) + Units.inchesToMeters(5),
+                    targetPosition.getRotation());
 
-            addCommands(Commands.parallel(new RunAnglerCommand(
-                    () -> Rotation2d.fromDegrees(angle.get()),
-                    angler, TerminationCondition.SETPOINT_REACHED),
-                    new DriveLockedRotCommand(drivebase, 0, () -> false)));
+            Pose2d drivebasePosition = drivebase.getOdometry().getEstimatedPosition();
+
+            double drivebaseAngle = Math.atan2(drivebasePosition.getY() - targetPosition.getY(),
+                    drivebasePosition.getX() - targetPosition.getX());
+
+            addCommands(
+                    Commands.parallel(
+                            new RunAnglerCommand(
+                                    () -> Rotation2d.fromDegrees(angle.get()),
+                                    angler, TerminationCondition.SETPOINT_REACHED),
+                            new DriveLockedRotCommand(drivebase, drivebaseAngle, () -> false)),
+                    RunManipulatorCommand.withMode(shooter, Modes.FORWARD).withTimeout(1),
+                    Commands.parallel(RunManipulatorCommand.withMode(intake, Modes.FORWARD).withTimeout(2),
+                            RunManipulatorCommand.withMode(shooter, Modes.FORWARD).withTimeout(3)));
 
         } catch (Exception ignored) {
 
